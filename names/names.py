1d693578932f53a6b53528ae3c8d757e6bbfbc03
mandihamza <mandihamza@gmail.com>
2020-09-01 16:48:12 -0600

    Refactored MVP to BST

--- names/names.py
+++ names/names.py
@@ -12,7 +12,9 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+
 # Replace the nested for loops below with your improvements
+
 # This solution is O(n²) quadratic
 # for name_1 in names_1:  # iterate through each name in name_1.txt one-by-one
 #     for name_2 in names_2:  # iterate through each name in name_2.txt one-by-one
@@ -20,10 +22,52 @@ duplicates = []  # Return the list of duplicates in this data structure
 #             duplicates.append(name_1)  # append name_1.txt to duplicates list
 
 # This solution is O(n) linear
-for name in names_1:  # iterate through each name in name_1.txt one-by-one
-    if name in names_2:  # check if name in name_1.txt is also in names_2.txt
-        if name not in duplicates:  # check if name is not yet in duplicates list
-            duplicates.append(name)  # append all found duplicate names to duplicates list
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left: BSTNode = None
+        self.right: BSTNode = None
+
+    def insert(self, value):
+        if value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                right_child = self.right
+                right_child.insert(value)
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                left_child = self.left
+                left_child.insert(value)
+
+    def contains(self, target):
+        if target > self.value:
+            if self.right == target:
+                return True
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+        if target < self.value:
+            if self.left == target:
+                return True
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            return True
+
+
+print_dupes = BSTNode('names')
+for name in names_1:
+    print_dupes.insert(name)
+
+for name in names_2:
+    if print_dupes.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
